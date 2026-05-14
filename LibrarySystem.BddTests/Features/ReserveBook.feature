301240a44bdf7ebfd3db1e2150faa9880893c6c6
andhot01 <andhot01@easv365.dk>
2026-05-14 18:14:43 +0200

    Added Cucumber tests for reserve book method

---
+++ LibrarySystem.BddTests/Features/ReserveBook.feature
@@ -0,0 +1,29 @@
+﻿Feature: Reserve a book
+
+  As a library member
+  I want to reserve a book that is currently checked out
+  So that I can borrow it when it becomes available
+
+  Background:
+    Given a user exists
+
+  Scenario: User reserves a book with no available copies
+    And a book exists with 0 available copies
+    And the user has not already borrowed the book
+    And the user has not already reserved the book
+    When the user attempts to reserve the book
+    Then the reservation should be successful
+    And the user should be added to the reservation queue for the book
+
+  Scenario Outline: User attempts to reserve a book
+    And a book exists with <available_copies> available copies
+    And the user has <loan_status> borrowed the book
+    And the user has <reservation_status> reserved the book
+    When the user attempts to reserve the book
+    Then the result should be <expected_result>
+
+    Examples:
+      | available_copies | loan_status | reservation_status | expected_result |
+      | 1                | not         | not                | BookAvailable   |
+      | 0                | already     | not                | AlreadyBorrowed |
+      | 0                | not         | already            | AlreadyReserved |
---
+++ LibrarySystem.BddTests/StepDefinitions/ReserveBookSteps.cs
@@ -0,0 +1,124 @@
+﻿using FluentAssertions;
+using LibrarySystem.Core.Interfaces;
+using LibrarySystem.Core.Models;
+using LibrarySystem.Core.Results;
+using LibrarySystem.Core.Services;
+using Moq;
+using Reqnroll;
+
+namespace LibrarySystem.BddTests.StepDefinitions;
+
+[Binding]
+public class ReserveBookSteps
+{
+    private readonly Mock<IBookRepository> _bookRepositoryMock = new();
+    private readonly Mock<IUserRepository> _userRepositoryMock = new();
+    private readonly Mock<ILoanRepository> _loanRepositoryMock = new();
+    private readonly Mock<IReservationRepository> _reservationRepositoryMock = new();
+
+    private readonly LibraryUser _user = new()
+    {
+        Id = 1,
+        Name = "Bob"
+    };
+    
+    private Book? _book;
+    private void SetupBookWithAvailableCopies(int availableCopies)
+    {
+        _book = new Book
+        {
+            Id = 1,
+            Title = "Test Book",
+            TotalCopies = 4,
+            AvailableCopies = availableCopies
+        };
+         _bookRepositoryMock.Setup(x => x.GetBook(1)).Returns(_book);
+    }
+    
+    private ReserveResult _result;
+
+    private LibraryService CreateService()
+    {
+        return new LibraryService(_userRepositoryMock.Object, _bookRepositoryMock.Object, 
+            _loanRepositoryMock.Object, _reservationRepositoryMock.Object);
+    }
+
+    
+    
+    [Given("a user exists")]
+    public void GivenAUserExists()
+    {
+        _userRepositoryMock.Setup(x => x.GetById(1)).Returns(_user);
+    }
+
+    [Given("a book exists with (.*) available copies")]
+    public void GivenABookExistsWithAvailableCopies(int availableCopies)
+    {
+        SetupBookWithAvailableCopies(availableCopies);
+    }
+
+    [Given("the user has (.*) borrowed the book")]
+    public void GivenTheUserHasBorrowedTheBook(string loanStatus)
+    {
+        if (loanStatus == "already")
+        {
+            _loanRepositoryMock.Setup(x => x.GetLoan(1, 1)).Returns(new Loan
+            {
+                UserId = 1,
+                BookId = 1
+            });
+        }
+        else
+        {
+            _loanRepositoryMock.Setup(x => x.GetLoan(1, 1)).Returns((Loan?)null);
+        }
+    }
+
+    [Given("the user has (.*) reserved the book")]
+    public void GivenTheUserHasReservedTheBook(string reservationStatus)
+    {
+        if (reservationStatus == "already")
+        {
+            _reservationRepositoryMock.Setup(x => x.GetReservationsForBook(1)).Returns(new List<Reservation>
+            {
+                new Reservation
+                {
+                    UserId = 1,
+                    BookId = 1,
+                    ReservedAt = DateTime.Now
+                }
+            });
+        }
+        else
+        {
+            _reservationRepositoryMock.Setup(x => x.GetReservation(1,1)).Returns((Reservation?) null);
+        }
+    }
+
+    [When("the user attempts to reserve the book")]
+    public void WhenTheUserAttemptsToReserveTheBook()
+    {
+        var service = CreateService();
+        _result = service.ReserveBook(1, 1);
+    }
+
+    [Then("the reservation should be succesful")]
+    public void ThenTheReservationShouldBeSuccesful()
+    {
+        _result.Should().Be(ReserveResult.Success);
+    }
+
+    [Then("the user should be added to the reservation queue for the book")]
+    public void ThenTheUserShouldBeAddedToTheReservationQueue()
+    {
+        _reservationRepositoryMock.Verify(x => 
+            x.Add(It.Is<Reservation>(r => r.UserId == 1 && r.BookId == 1)), Times.Once);
+    }
+
+    [Then("the result should be (.*)")]
+    public void ThenTheResultShouldBe(string expectedResult)
+    {
+        var expected = Enum.Parse<ReserveResult>(expectedResult);
+        _result.Should().Be(expected);
+    }
+}
